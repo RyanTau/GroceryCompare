09c6bb1ff6c7958b5ac3a0704fa0c20df9447882
SoitaNoita <ryan18.tauhid20@gmail.com>
2022-06-27 02:21:59 +1000

    woolworths start

--- websites.py
+++ websites.py
@@ -9,6 +9,7 @@ import re
 def coles(search):
     url = f'https://shop.coles.com.au/a/national/everything/search/{search}?pageNumber=1'
     path = r'C:\Users\Ryan1\Downloads\chromedriver_win32 (1)\chromedriver.exe'
+    path = r"C:\Users\Ryan1\Downloads\chromedriver_win32 (2)\chromedriver.exe"
     driver = webdriver.Chrome(path)
     driver.get(url)
     # Name, Price, Quantity
@@ -29,6 +30,34 @@ def coles(search):
         json_out["coles"]= {'title': title, 'price': price, 'quantity': quantity}
         print(price)
         print("\n")
+    return json_out
 
+def woolworth(search):
+    url = f"https://www.woolworths.com.au/shop/search/products?searchTerm={search}"
+    
+    path = r'C:\Users\Ryan1\Downloads\chromedriver_win32 (1)\chromedriver.exe'
+    path = r"C:\Users\Ryan1\Downloads\chromedriver_win32 (2)\chromedriver.exe"
+    driver = webdriver.Chrome(path)
+    driver.get(url)
+    # Name, Price, Quantity
+    json_out = {}
+    out = driver.find_elements(By.CLASS_NAME, 'shelfProductTile-information')
+    out = [x.text for x in out]
+    for i in out:
+        title = re.search(r'(.*)\n', i)
+        title= title.group(0)
 
-coles("milk")
+        price = re.search(r'sponsored product\n(.*)\n', i)
+        if price is None:
+            price = re.search(r'.*\n.*\n(.*)', i)
+            price = price.group(0)
+       
+        quantity = re.search(r'\n(.*)\n', i)
+        quantity = quantity.group(0)
+        json_out["coles"]= {'title': title, 'price': price, 'quantity': quantity}
+        print(price)
+        print("\n")
+
+woolworth("chocolate")
+
+#coles("milk")
