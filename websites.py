86d0f38c81913d831bbfdf64457615d65182d0c4
SoitaNoita <ryan18.tauhid20@gmail.com>
2022-06-29 01:42:57 +1000

    added images

--- websites.py
+++ websites.py
@@ -1,12 +1,31 @@
 import json
 from tkinter.messagebox import NO
 from bs4 import BeautifulSoup
-from numpy import append
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import requests
-import re
+import requests, urllib, re, time
+import numpy as np
+from PIL import Image
+from io import BytesIO
 
+
+
+def coles_images(url,driver,test):
+    out =driver.find_elements(By.XPATH,r"//div[@class='product-image-container']//img")
+    out = [x.get_attribute('src') for x in out]
+    y = 0
+    for x in out:
+        response = requests.get(x)
+        bytes_img = BytesIO(response.content)
+        # img = Image.open(bytes_img)
+        # img.show()
+        y += 1
+    return out
+
+def woolworth_images(url,driver):
+    out = driver.find_elements(By.CLASS_NAME, 'shelfProductTile-image')
+    out = [x.get_attribute('src') for x in out]
+    return out
 
 def coles(search,json_out,num_results=5):
     url = f'https://shop.coles.com.au/a/national/everything/search/{search}?pageNumber=1'
@@ -14,12 +33,16 @@ def coles(search,json_out,num_results=5):
     path = r"C:\Users\Ryan1\Downloads\chromedriver_win32 (2)\chromedriver.exe"
     driver = webdriver.Chrome(path)
     driver.get(url)
-    # Name, Price, Quantity
-    list_json = []
     out = driver.find_elements(By.CLASS_NAME, 'product-main-info')
     out = [x.text for x in out]
+
+    out_img =driver.find_elements(By.XPATH,r"//div[@class='product-image-container']//img")
+    out_img = [x.get_attribute('src') for x in out_img]
+
+    # Name, Price, Quantity
+    TPQI = []
     iterator = 0
-    for i in out:
+    for i,x in zip(out,out_img):
         # print(i)
         # print("\n")
         title = re.search(r'(.*)\n', i)
@@ -31,14 +54,20 @@ def coles(search,json_out,num_results=5):
             price = price.group(1)
         else:
             price = price.group(1)
-        if iterator == num_results:
-            break
+
         iterator += 1
         quantity = re.search(r'\n(.*)\n', i)
         quantity = quantity.group(1)
-        list_json.append({'title': title, 'price': price, 'quantity': quantity})
 
-    json_out["coles"] = list_json
+        response = requests.get(x)
+        bytes_img = BytesIO(response.content)
+        
+        TPQI.append([title,price,quantity,bytes_img])
+        #list_json.append({'title': title, 'price': price, 'quantity': quantity})
+    TPQI.sort(key=lambda x: x[1])
+    TPQI = TPQI[:num_results]
+    json_out["coles"] = TPQI
+
     return json_out
 
 def woolworth(search,json_out,num_results=5):
@@ -49,13 +78,18 @@ def woolworth(search,json_out,num_results=5):
     driver = webdriver.Chrome(path)
     driver.get(url)
 
-    # Name, Price, Quantity
     out = driver.find_elements(By.CLASS_NAME, 'shelfProductTile-information')
     out = [x.text for x in out]
+    
+    out_img =driver.find_elements(By.XPATH,r"//div[@class='shelfProductTile-imageWrapper']//img")
+    out_img = [x.get_attribute('src') for x in out_img]
+    
+    # Name, Price, Quantity
+    TPQI = []
     list_json = []
     iterator = 0
-    for i in out:
-        
+    for i,x in zip(out,out_img):
+        print(iterator)
         title = re.search(r'(.*)\n', i)
         title= title.group(1)
 
@@ -83,14 +117,19 @@ def woolworth(search,json_out,num_results=5):
             dpq = "N/A"
             qty = "N/A"
             units = "N/A"
-        if iterator == num_results:
-            break
+        response = requests.get(x)
+        print(x)
+        bytes_img = BytesIO(response.content)
+        TPQI.append([title,price,quantity,bytes_img])
         iterator += 1
-        list_json.append({'title': title, 'price': price, 'quantity': quantity})
-        json_out["woolworth"] = list_json 
+
+    TPQI.sort(key=lambda x: x[1])
+    TPQI = TPQI[:num_results] 
+    json_out["woolworth"] = TPQI 
     return json_out
 
 json_out = {}
-json_out = (coles("chocolate",json_out))
+#json_out = (coles("chocolate",json_out))
+#print(json_out)
 print(woolworth("chocolate",json_out))
 #coles("milk")
